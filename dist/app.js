1f0ff2f1144023ba44be0e384911db5c1c88639d
Maksym <siroshtan.max1991ua@gmail.com>
2023-10-22 15:02:33 +0300

    Header layout

--- dist/app.js
+++ dist/app.js
@@ -39,9 +39,24 @@
       this.div.innerHTML = "";
       this.div.classList.add("header");
       this.div.innerHTML = `
-      <div>
+      <div class="logo">
         <img src="./src/static/logo.svg" alt="Логотип" />
       </div>
+      <ul class="menu">
+        <li class="menu__item">
+          <a class="menu__link" href="#">
+          <img src="./src/static/search.svg" alt="Логотип" />
+          Поиск книг
+          </a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">
+          <img src="./src/static/favorites.svg" alt="Логотип" />
+          Избранное
+          </a>
+          <span>${this.appState.favorites.length}</span>
+        </li>
+      </ul>
     `;
       return this.div;
     }
